681b688d1778582951f023c7cc353aa0c2f642c2
Alok <optimisticclown098@gmail.com>
2026-06-23 20:46:25 +0530

    Time: 154 ms (26.31%) | Memory: 153.3 MB (16.55%) - LeetSync

---
+++ 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
+        
+        multiset<int> s;
+        long long i = 0, j = 0, robots = 0, cost = 0;
+
+        while(j < chargeTimes.size()){
+
+            s.insert(chargeTimes[j]);
+            cost += runningCosts[j];
+
+            while(!s.empty() && (*s.rbegin() + (j-i+1) * cost) > budget){
+
+                s.erase(s.find(chargeTimes[i]));
+                cost -= runningCosts[i];
+                i++;
+            }
+            robots = max(robots, j-i+1);
+            j++;
+        }
+        return robots;
+    }
+};
