59a057e1bcc4aa67be6f40d0ab28a25afe7b9b91
Alok <optimisticclown098@gmail.com>
2026-07-23 19:05:06 +0530

    Time: 21 ms (69.41%) | Memory: 77.4 MB (77.16%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -5,25 +5,25 @@ public:
         deque<int> q1;
         deque<int> q2;
 
-        int i = 0, j = 0, count = 0;
-
+        int i=0, j=0, count = 0;
         while(j < nums.size()){
 
-            while(!q1.empty() && nums[q1.back()] < nums[j])
+            while(!q1.empty() && nums[q1.back()]<nums[j])
             q1.pop_back();
             q1.push_back(j);
 
-            while(!q2.empty() && nums[q2.back()] > nums[j])
+            while(!q2.empty() && nums[q2.back()]>nums[j])
             q2.pop_back();
             q2.push_back(j);
 
             while(nums[q1.front()] - nums[q2.front()] > limit){
 
-                if(q1.front() == i) q1.pop_front();
-                if(q2.front() == i) q2.pop_front();
+                if(i==q1.front()) q1.pop_front();
+                if(i==q2.front()) q2.pop_front();
                 i++;
             }
-            count = max(count, j-i+1);
+
+            count = max(count,j-i+1);
             j++;
         }
         return count;
