f45290805c93690400c56067b7e2159d80aa9fa0
Alok <optimisticclown098@gmail.com>
2026-07-27 15:47:43 +0530

    Time: 10 ms (64.65%) | Memory: 20.7 MB (85.97%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -6,35 +6,36 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        int r[4] = {-1, 1, 0, 0};
-        int c[4] = {0, 0, 1, -1};
-        
+        int r[4] = {-1,1,0,0};
+        int c[4] = {0,0,1,-1};
+
         if(dp[i][j] != -1) return dp[i][j];
 
-        int ans = 1;
-
-        for(int k = 0; k<4; k++){
-
-            int x = i + r[k];
-            int y = j + c[k];
-
+        int path = 1;
+        for(int k=0; k<4; k++){
+            
+            int x = i+r[k];
+            int y = j+c[k];
             if(x>=0 && x<n && y>=0 && y<m && grid[x][y] > grid[i][j])
-            ans = max(ans, 1 + memo(grid, x, y, dp));
+            path = max(path, 1 + memo(grid,x,y,dp));
         }
-        return dp[i][j] = ans;
+
+        return dp[i][j] = path;
     }
 
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         
         int n = matrix.size();
         int m = matrix[0].size();
+        int path = 0;
         vector<vector<int>> dp(n, vector<int> (m, -1));
 
-        int path = 0;
-        for(int i = 0; i<n; i++)
-        for(int j = 0; j<m; j++)
-        path = max(path, memo(matrix, i, j, dp));
-        
+        for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++){
+
+            path = max(path, memo(matrix, i, j, dp));
+        }
+
         return path;
     }
 };
