d8f3b42afb0fc93f5b518df8f39495eded984969
Alok <optimisticclown098@gmail.com>
2026-08-04 10:10:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (52.01%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,10 +4,7 @@ public:
         
         sort(nums.begin(), nums.end());
         int median, ops = 0;
-        if(nums.size() & 1)
         median = nums[(nums.size()-1)/2];
-        else
-        median = (nums[nums.size()/2] + nums[nums.size()/2-1] )/2;
 
         for(auto &it: nums)
         ops += abs(median-it);
