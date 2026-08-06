efcc451755c6ba9d12fa7a4ea0a892770ad24669
Alok <optimisticclown098@gmail.com>
2026-08-06 10:56:54 +0530

    Time: 1 ms (63.97%) | Memory: 9.7 MB (17.63%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -3,34 +3,31 @@ public:
 
     bool memo(auto &s, auto &p, int i, int j, auto &dp){
 
-        if(i>=s.size() && j>=p.size()) return true;
+        if(i==s.size() && j==p.size()) return true;
         if(j >= p.size()) return false;
-
-        if(i>=s.size()){
+        if(i==s.size()){
 
             if(p[p.size()-1] != '*') return false;
+            for(int k=j+1; k<p.size()-1; k+=2)
+            if(p[k] != '*') return false;
 
-            for(int k = j+1; k<p.size()-1; k+=2){
-
-                if(p[k]!='*')
-                return false;
-            }
             return true;
         }
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int match = false, match2 = false, skip = false;
-        if((s[i] == p[j] || p[j]=='.') && (p[j+1] != '*'))
-        match = match || memo(s, p, i+1, j+1, dp);
+        bool match1 = false, match2 = false, skip = false;
 
-        if((s[i] == p[j] || p[j]=='.') && p[j+1] == '*')
+        if((s[i]==p[j] || p[j] == '.') && p[j+1] != '*')
+        match1 = match1 || memo(s, p, i+1, j+1, dp);
+
+        if((s[i]==p[j] || p[j] == '.') && p[j+1] == '*')
         match2 = match2 || memo(s, p, i+1, j, dp);
 
         if(p[j+1] == '*')
         skip = skip || memo(s, p, i, j+2, dp);
 
-        return dp[i][j] = match || match2 || skip;
+        return dp[i][j] = match1 || match2 || skip;
     }
 
     bool isMatch(string s, string p) {
