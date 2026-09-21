9c99d81e9295cbcb17580eb3855c10811dccad82
Alok <optimisticclown098@gmail.com>
2026-09-21 16:07:47 +0530

    Time: 229 ms (48.02%) | Memory: 194.2 MB (39.18%) - LeetSync

--- 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
@@ -1,60 +1,55 @@
 class Solution {
 public:
 
-    bool canCross(int n, int m, vector<vector<int>>& cells, int mid){
+    bool canCross(int mid, int row, int col, auto &cells){
 
-        vector<vector<int>> v(n, vector<int> (m, 0));
+        vector<vector<int>> grid(row, vector<int>(col, 0));
+        for(int i = 0; i < mid; i++)
+        grid[cells[i][0]-1][cells[i][1]-1] = 1;
+
         int r[4] = {-1, 1, 0, 0};
         int c[4] = {0, 0, 1, -1};
 
-        for(int i = 0; i<mid; i++)
-        v[cells[i][0]-1][cells[i][1]-1] = 1;
-
         queue<pair<int,int>> q;
-        for(int i = 0; i<m; i++){
-
-            if(!v[0][i]){
-                q.emplace(0,i);
-                v[0][i] = 1;
-            }
-        }
+        for(int i=0; i<col; i++)
+        if(!grid[0][i]) q.emplace(0,i);
 
         while(!q.empty()){
 
             auto[ni, nj] = q.front();
-            if(ni == n-1) return true;
+            if(ni==row-1) return true;
             q.pop();
 
-            for(int k=0; k<4; k++){
+            for(int k=0; k<=3; k++){
 
                 int x = ni+r[k];
                 int y = nj+c[k];
 
-                if(x>=0 && x<n && y>=0 && y<m && !v[x][y]){
+                if(x>=0 && x<row && y>=0 && y<col && !grid[x][y]){
 
                     q.emplace(x,y);
-                    v[x][y] = 1;
+                    grid[x][y] = 1;
                 }
             }
         }
-        return false;        
+        return false;
     }
 
     int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
         
-        int lo = 1, hi = cells.size(), mid, ans=0;
+        int lo=0, hi=cells.size(), lastDay;
 
-        while(lo <= hi){
+        while(lo<=hi){
 
-            mid = lo + (hi-lo)/2;
+            int mid = lo+(hi-lo)/2;
 
-            if(canCross(row, col, cells, mid)){
-                ans = mid;
+            if(canCross(mid, row, col, cells)){
+                lastDay = mid;
                 lo = mid+1;
             }
             else
             hi = mid-1;
         }
-        return ans;
+        return lastDay;
     }
 };
